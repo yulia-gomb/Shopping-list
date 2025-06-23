b3ee60da1eb9cb0f5a64bd7b06d60da546d9ec6a
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-23 17:08:25 +0300

    Add factory for creating state objects

---
+++ src/factories/StateFactory.ts
@@ -0,0 +1,17 @@
+import { ActiveState, PurchasedState, RemovedState, type State } from "../models/State.ts";
+
+export class StateFactory {
+    private static stateMap: Map<string, new () => State> = new Map([
+        ['Active', ActiveState],
+        ['Purchased', PurchasedState],
+        ['Removed', RemovedState],
+    ]);
+
+    static getState(stateName: string): State {
+        const StateClass = this.stateMap.get(stateName);
+        if (!StateClass) {
+            throw new Error(`State "${stateName}" is not recognized. Available states: ${Array.from(this.stateMap.keys()).join(', ')}`);
+        }
+        return new StateClass();
+    }
+}
