46d33baf87ed04505c458b0788955e4b062ed97b
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-27 00:38:26 +0300

    The command to delete the product

---
+++ src/patterns/RemoveItemCommand.ts
@@ -0,0 +1,26 @@
+import { Observable } from '../services/Observer';
+import { LocalStorageService } from '../services/LocalStorageService';
+import type { Command } from "./Command.ts";
+import type { ShoppingItem } from "../models/ShoppingItem.ts";
+
+export class RemoveItemCommand implements Command {
+    private shoppingList: Observable<ShoppingItem[]>;
+    private readonly item: ShoppingItem;
+
+    constructor(shoppingList: Observable<ShoppingItem[]>, item: ShoppingItem) {
+        this.shoppingList = shoppingList;
+        this.item = item;
+    }
+
+    execute(): void {
+        const updatedList = this.shoppingList.getData().filter((i) => i.id !== this.item.id);
+        this.shoppingList.setData(updatedList);
+        LocalStorageService.save(updatedList);
+    }
+
+    undo(): void {
+        const updatedList = [...this.shoppingList.getData(), this.item];
+        this.shoppingList.setData(updatedList);
+        LocalStorageService.save(updatedList);
+    }
+}
